92c020bc308f68541f90e0fdd7acfe16cae794b1
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2022-01-03 13:47:51 -0300

    primeira versão escola

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RO-Cursos</title>
+</head>
+<body>
+    <h1>Olá PhP!</h1>
+    
+</body>
+</html>
