b28c0c9be55b726683cc8b223be8a752e0ff7c12
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2022-01-11 13:34:58 -0300

    conectado com o banco

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "escola";
+
+    $conn = mysqli_connect($server, $user, $password, $dbname);
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,8 +14,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RO-Cursos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
         <nav class="navbar navbar-expand-lg navbar-light navbarback">
