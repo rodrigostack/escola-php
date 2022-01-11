9632fd9a116b8818f5a8a1ba5d85177a736688c0
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2022-01-11 13:39:26 -0300

    pagina conexão com require na index

---
+++ conection.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "escola";
+
+    $conn = mysqli_connect($server, $user, $password, $dbname);
+?>
--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $dbname = "escola";
-
-    $conn = mysqli_connect($server, $user, $password, $dbname);
+<?php 
+    require 'conection.php';
 ?>
 
 <!DOCTYPE html>
