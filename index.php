33b1cdd853538d1f731c37ab984bd749050f4e7b
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2022-01-13 12:49:24 -0300

    Com icones font awesome

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RO-Cursos</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
@@ -38,6 +41,7 @@
                 </ul>
                 </li>
             </ul>
+          
             </div>
         </div>
         </nav>
@@ -75,9 +79,12 @@
                         <td><?php echo $curso ?></td>
                         <td><?php echo $cidade ?></td>
                         <td><?php echo $pgto ?></td>
+                        <td><a href="#"><h5><i class="fa-solid fa-pen-to-square"></i></h5></a></td>
+                        <td><a href="#"><h5><i class="fas fa-trash-alt"></i></h5></a></td>
                                    
                     </tr>
                     <?php  }    ?>
+                    
                    
                 
                 </tbody>
@@ -85,7 +92,7 @@
             </div>
 
         </main>
-    
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js" integrity="sha512-fzff82+8pzHnwA1mQ0dzz9/E0B+ZRizq08yZfya66INZBz86qKTCt9MLU0NCNIgaMJCgeyhujhasnFUsYMsi0Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
